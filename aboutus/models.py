71e71de48befa8862afa23512fdd3b44de32f8de
Sherhif <mohammedsherif450@gmail.com>
2024-03-04 14:47:29 +0000

    models

--- aboutus/models.py
+++ aboutus/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class AboutUs(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    our_mission = models.TextField()
+    our_vision = models.TextField()
+    why_us = models.TextField()
+
+    def __str__(self):
+        return f"{self.title}"
+    # def __str__(self):
+    #     return f"{self.description}"
+    # def __str__(self):
+    #     return f"{self.our_mission}"
+    # def __str__(self):
+    #     return f"{self.our_vision}"
+    # def __str__(self):
+    #     return f"{self.why_us}"
