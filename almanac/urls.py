f8e6d8f757527ccd24599a1ec2b03e42a793b016
Sherhif <mohammedsherif450@gmail.com>
2024-03-14 16:01:23 +0000

    blog images populated from backend

--- almanac/urls.py
+++ almanac/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import include, path
 from contactus import views
 from dashboard import views as dashboardviews
+from django.conf import settings
+from django.conf.urls.static import static
 
 extra_patterns = [
     path("institutions/", dashboardviews.institutions),
@@ -44,3 +46,5 @@ urlpatterns = [
 
 
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
